326c4948c5577addc8113caa8178983a88e5481e
hopwesley <ws@sinoc.io>
2020-09-19 14:42:31 +0800

    save

---
+++ Pirate/common/Wallet.swift
@@ -0,0 +1,15 @@
+//
+//  Wallet.swift
+//  Pirate
+//
+//  Created by wesley on 2020/9/19.
+//  Copyright © 2020 hyperorchid. All rights reserved.
+//
+
+import Foundation
+import IosLib
+
+
+class Wallet:NSObject{
+        
+}
